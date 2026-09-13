295933f44fdd33d6650195c333f55da8cf40870e
kaeruqi <jan.blad10@gmail.com>
2026-09-13 11:53:39 +0200

    Add files via upload

---
+++ bladowski_zewnetrzny.js
@@ -0,0 +1,8 @@
+function okno_zamknij()
+ {
+ window.close() }
+document.write("moj numer w dzienniku to 1"+"<br>");
+document.write("pierwszy wiersz to: 16  tak  nie  6"+"<br>");
+document.write("drugi wiersz to:    13  tak  nie  6"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#00FFFF").bold().fontsize(6)+"<br>");
+document.write(document.lastModified.fontcolor("navy").bold().fontsize(6)+"<br>");
